24722b482e0cf12dd452a911a043fd03f8ee28e7
renz-pc <renz9204@gmail.com>
2024-07-18 21:12:11 +0800

    BAT_1

---
+++ BSCRIPT01.bat
@@ -0,0 +1,9 @@
+start https://uc-bcf.instructure.com
+start https://youtube.com
+start https://music.youtube.com
+start https://messenger.com
+start https://facebook.com
+start calc.exe
+start notepad.exe
+shutdown -s -t 30
+echo Shutting down in 30 seconds
